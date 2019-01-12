63a619150856a416a44544f15806d8c0032b797d
AHaliq <liqo12@hotmail.com>
2019-01-12 19:01:33 +0800

    form to submit puzzles and replace text with solution

--- site/main.js
+++ site/main.js
@@ -1,1 +1,22 @@
-alert("it works");
+function submitFunc() {
+    var data = document.getElementById("textAr").value.replace(/\n/g, "nn");
+    data += "nnenn";
+    data = "text="+data;
+    console.log(`Puzzle: ${data}`);
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4) {
+        //console.log(this.responseText);
+        document.getElementById("textAr").value = this.responseText;
+    }
+    });
+
+    xhr.open("POST", "http://localhost:8000/solve");
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.setRequestHeader("cache-control", "no-cache");
+    xhr.setRequestHeader("Postman-Token", "cb805568-b265-4674-a928-9c4e9c200bbd");
+
+    xhr.send(data);
+}
