31594036f207302f67c6b05a30d7dd3d887c08b4
AHaliq <liqo12@hotmail.com>
2019-01-12 19:54:31 +0800

    changed to fetch api

--- site/main.js
+++ site/main.js
@@ -2,20 +2,17 @@ function submitFunc() {
     var data = document.getElementById("textAr").value.replace(/\n/g, "nn");
     data = "text="+data+"nnenn";
     
-    var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
-
-    xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === 4) {
-        //console.log(this.responseText);
-        document.getElementById("textAr").value = this.responseText;
-    }
-    });
-
-    xhr.open("POST", "http://localhost:8000/solve");
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.setRequestHeader("cache-control", "no-cache");
-    xhr.setRequestHeader("Postman-Token", "cb805568-b265-4674-a928-9c4e9c200bbd");
-
-    xhr.send(data);
+    fetch("http://localhost:8000/solve",{
+        headers:{
+            "content-type": "application/x-www-form-urlencoded",
+            "cache-control": "no-cache",
+            "Postman-Token": "cb805568-b265-4674-a928-9c4e9c200bbd"
+        },
+        body:data,
+        method:"POST"
+    })
+    .then(data=>data.text())
+    .then(res=>{
+        document.getElementById("textAr").value = res;
+    }).catch(err=>console.log(error));
 }
