0112967f3558c6582b33a0b10bad516c6830ca9a
AHaliq <liqo12@hotmail.com>
2019-07-07 14:36:44 +0800

    include kubectl to query service address

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM haskell:8
 
+COPY --from=lachlanevenson/k8s-kubectl:v1.10.3 /usr/local/bin/kubectl /usr/local/bin/kubectl
+
 EXPOSE 8000
 
 RUN mkdir -p /var/www/app
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 if [ -n "$IP_ADDRESS" ]; then
     echo "const URL = \"$IP_ADDRESS\";" >> ./site/address.js
+elif [ -n "$SERVICE" ]; then
+    echo "const URL = \"$(kubectl get svc $SERVICE -o template --template '{{(index .status.loadBalancer.ingress 0).ip}}')\";" >> ./site/address.js
 else
-    echo 'No environment variable IP_ADDRESS, client cant reach server functions'
+    echo 'No environment variable IP_ADDRESS nor SERVICE, client cant reach server functions'
 fi
 exec "$@"
