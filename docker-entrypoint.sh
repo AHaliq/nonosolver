b16ed3decc74c6633b02c97360de65c6301fc5f3
AHaliq <liqo12@hotmail.com>
2019-07-07 19:20:44 +0800

    replace url file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 if [ -n "$IP_ADDRESS" ]; then
-    echo "const URL = \"http://$IP_ADDRESS/\";" >> ./site/address.js
+    echo "const URL = \"http://$IP_ADDRESS/\";" > ./site/address.js
 elif [ -n "$SERVICE" ]; then
     IP_ADDRESS="$(kubectl get service $SERVICE --output yaml | grep 'ip:\|targetPort:' | awk -F': ' '{ print $2 }' | tac | sed 'N;s/\n/:/')"
-    echo "const URL = \"http://$IP_ADDRESS/\";" >> ./site/address.js
+    echo "const URL = \"http://$IP_ADDRESS/\";" > ./site/address.js
 else
     echo 'No environment variable IP_ADDRESS nor SERVICE, client cant reach server functions'
 fi
